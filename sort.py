3cdee86a2f125ceabe973029bce6b41c331860c6
UreshiiSushi <merieru@gmail.com>
2023-09-18 17:50:23 +0200

    added unpack method

--- sort.py
+++ sort.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import os
 import shutil
 import re
+from zipfile import ZipFile
 
 CATEGORIES = {"audio": [".mp3", ".wav", ".flac", ".wma"],
               "video": [".mkv", ".avi", ".mov", ".mp4"],
@@ -10,6 +11,21 @@ CATEGORIES = {"audio": [".mp3", ".wav", ".flac", ".wma"],
               "archives": [".zip", ".gz", ".tar"],
               "docs": [".doc", ".docx", ".txt", ".pdf", ".xlsx", ".pptx"]
               }
+
+file_list = {"audio": [],
+             "video": [],
+             "images": [],
+             "archives": [],
+             "docs": [],
+             "other": []
+             }
+ext_list = {"audio": [],
+            "video": [],
+            "images": [],
+            "archives": [],
+            "docs": [],
+            "other": []
+            }
 
 CYRILLIC_SYMBOLS = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяєіїґ"
 TRANSLATION = ("a", "b", "v", "g", "d", "e", "e", "j", "z", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u",
@@ -20,14 +36,23 @@ for c, l in zip(CYRILLIC_SYMBOLS, TRANSLATION):
     TRANS[ord(c.upper())] = l.upper()
 
 
-def normalize(file: Path):
+def normalize(file: Path) -> str:
     new_name = file.name.translate(TRANS)
     new_name = re.sub(r"[^a-z0-9A-Z.]", "_", new_name)
     return new_name
 
 
-def write_in_file(list: list, path: Path):
-    ...
+def write_in_file(file_list: dict, ext_list: dict, path: Path) -> None:
+    for k, v in file_list.items():
+        text = "\n".join(v)
+        file_path = path.joinpath(k).joinpath(k + ".txt")
+        with open(file_path, "w") as fh:
+            fh.write(text)
+    for k, v in ext_list.items():
+        text = "\n".join(v)
+        file_path = path.joinpath(k).joinpath(k + "_ext.txt")
+        with open(file_path, "w") as fh:
+            fh.write(text)
 
 
 def get_category(file: Path) -> str:
@@ -35,25 +60,32 @@ def get_category(file: Path) -> str:
     for cat, exts in CATEGORIES.items():
         if ext in exts:
             return cat
-    return "Other"
+    return "other"
 
 
-def unpack_archive(path: Path):
-    ...
+def unpack_archive(directory: Path, archive: Path) -> None:
+    dir_name = archive.name.rstrip(archive.suffix)
+    unpack_path = directory.joinpath(dir_name)
+    if archive.suffix.lower() == ".zip":
+        with ZipFile(archive, "r") as zObj:
+            zObj.extractall(unpack_path)
+    else:
+        shutil.unpack_archive(archive, unpack_path, archive.suffix)
 
 
 def move_file(file: Path, category: str, root_dir: Path) -> None:
     target_dir = root_dir.joinpath(category)
     if not target_dir.exists():
         target_dir.mkdir()
-        # file_name = category + ".txt"
-        # with open(target_dir.joinpath(file_name), "w") as fh:
-        #     ...
     new_path = target_dir.joinpath(normalize(file))
     if not new_path.exists():
         file.replace(new_path)
+        if new_path.name not in file_list[category]:
+            file_list[category].append(new_path.name)
+        if new_path.suffix not in ext_list[category]:
+            ext_list[category].append(new_path.suffix)
         if category == "archives":
-            unpack_archive(new_path)
+            unpack_archive(target_dir, new_path)
 
 
 def sort_folder(path: Path) -> None:
@@ -63,10 +95,10 @@ def sort_folder(path: Path) -> None:
             move_file(i, category, path)
 
 
-def delete_empty_folders(path: Path):
-    for i in path.glob("**/*"):
+def delete_empty_folders(path: Path) -> None:
+    for i in path.rglob("**/*"):
         if i.is_dir() and not os.listdir(i):
-            i.rmdir()
+            shutil.rmtree(i)
 
 
 def main() -> str:
@@ -80,6 +112,7 @@ def main() -> str:
 
     sort_folder(path)
     delete_empty_folders(path)
+    write_in_file(file_list, ext_list, path)
 
     return "All Ok"
 
